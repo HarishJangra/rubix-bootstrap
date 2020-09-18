c904dbb7f7c9c2b3a311c4d1d16b261550edf300
Haish Jangra <harish0212@yahoo.com>
2020-09-18 11:53:28 +0530

    pagination

--- dist/lib/Pagination.js
+++ dist/lib/Pagination.js
@@ -4,10 +4,93 @@ Object.defineProperty(exports, "__esModule", {
   value: true
 });
 
+var _objectWithoutProperties2 = require('babel-runtime/helpers/objectWithoutProperties');
+
+var _objectWithoutProperties3 = _interopRequireDefault(_objectWithoutProperties2);
+
+var _react = require('react');
+
+var _react2 = _interopRequireDefault(_react);
+
 var _Pagination = require('react-bootstrap/lib/Pagination');
 
 var _Pagination2 = _interopRequireDefault(_Pagination);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-exports.default = _Pagination2.default;
+exports.default = function (_ref) {
+  var prev = _ref.prev,
+      next = _ref.next,
+      first = _ref.first,
+      last = _ref.last,
+      ellipsis = _ref.ellipsis,
+      boundaryLinks = _ref.boundaryLinks,
+      items = _ref.items,
+      maxButtons = _ref.maxButtons,
+      activePage = _ref.activePage,
+      onSelect = _ref.onSelect,
+      rest = (0, _objectWithoutProperties3.default)(_ref, ['prev', 'next', 'first', 'last', 'ellipsis', 'boundaryLinks', 'items', 'maxButtons', 'activePage', 'onSelect']);
+
+  var onPrev = function onPrev() {
+    activePage !== 1 && onSelect(activePage - 1);
+  };
+
+  var onNext = function onNext() {
+    activePage !== items && onSelect(activePage + 1);
+  };
+
+  var firstEllp = (items > activePage + 2 || items >= activePage - 2) && activePage > 4;
+  var secondEllp = items > activePage + 2 && activePage < items;
+  var arr = getArary(items, activePage);
+
+  return _react2.default.createElement(
+    _Pagination2.default,
+    rest,
+    prev && _react2.default.createElement(_Pagination2.default.Prev, { disabled: activePage === 1, onClick: onPrev }),
+    _react2.default.createElement(
+      _Pagination2.default.Item,
+      {
+        active: activePage === 1,
+        onClick: function onClick() {
+          onSelect(1);
+        }
+      },
+      1
+    ),
+    firstEllp && _react2.default.createElement(_Pagination2.default.Ellipsis, null),
+    _.map(arr, function (i) {
+      return _react2.default.createElement(
+        _Pagination2.default.Item,
+        {
+          onClick: function onClick() {
+            onSelect(i);
+          },
+          active: i === activePage,
+          key: i
+        },
+        i
+      );
+    }),
+    secondEllp && _react2.default.createElement(_Pagination2.default.Ellipsis, null),
+    _react2.default.createElement(
+      _Pagination2.default.Item,
+      {
+        onClick: function onClick() {
+          onSelect(items);
+        },
+        active: activePage === items
+      },
+      items
+    ),
+    next && _react2.default.createElement(_Pagination2.default.Next, { disabled: activePage === items, onClick: onNext })
+  );
+};
+
+var getArary = function getArary(items, active) {
+  var arr = [];
+
+  for (var i = active - 2; i <= active + 2; i++) {
+    if (i > 1 && i < items) arr.push(i);
+  }
+  return arr;
+};
--- src/Pagination.js
+++ src/Pagination.js
@@ -1,3 +1,81 @@
+import React from 'react'
 import Pagination from 'react-bootstrap/lib/Pagination';
 
-export default Pagination;
+export default ({
+    prev,
+    next,
+    first,
+    last,
+    ellipsis,
+    boundaryLinks,
+    items,
+    maxButtons,
+    activePage,
+    onSelect,
+    ...rest
+  }) => {
+    const onPrev = () => {
+      activePage !== 1 && onSelect(activePage - 1);
+    };
+  
+    const onNext = () => {
+      activePage !== items && onSelect(activePage + 1);
+    };
+  
+    let firstEllp =
+      (items > activePage + 2 || items >= activePage - 2) && activePage > 4;
+    let secondEllp = items > activePage + 2 && activePage < items;
+    let arr = getArary(items, activePage);
+  
+    return (
+      <Pagination {...rest}>
+        {prev && <Pagination.Prev disabled={activePage === 1} onClick={onPrev} />}
+        <Pagination.Item
+          active={activePage === 1}
+          onClick={() => {
+            onSelect(1);
+          }}
+        >
+          {1}
+        </Pagination.Item>
+  
+        {firstEllp && <Pagination.Ellipsis />}
+        {_.map(arr, (i) => {
+          return (
+            <Pagination.Item
+              onClick={() => {
+                onSelect(i);
+              }}
+              active={i === activePage}
+              key={i}
+            >
+              {i}
+            </Pagination.Item>
+          );
+        })}
+        {secondEllp && <Pagination.Ellipsis />}
+  
+        <Pagination.Item
+          onClick={() => {
+            onSelect(items);
+          }}
+          active={activePage === items}
+        >
+          {items}
+        </Pagination.Item>
+        {next && (
+          <Pagination.Next disabled={activePage === items} onClick={onNext} />
+        )}
+      </Pagination>
+    );
+  };
+  
+  const getArary = (items, active) => {
+    let arr = [];
+  
+    for (let i = active - 2; i <= active + 2; i++) {
+      if (i > 1 && i < items) arr.push(i);
+    }
+    return arr;
+  };
+  
